425766333c1e9afdfbe3d565aa4e8ee583441c5a
Liuba <kundasl1@gmail.com>
2022-12-01 22:15:31 +0300

    hw_4

--- web_service_with_TODO_notes/users/views.py
+++ web_service_with_TODO_notes/users/views.py
@@ -1,4 +1,7 @@
 #from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.request import Request
+
 from .models import User
 from .serializers import UserModelSerializer
 from rest_framework.views import APIView
@@ -17,12 +20,25 @@ class UserAPIView(APIView):
     #queryset = User.objects.all()
     #serializer_class = UserModelSerializer
 
-    def get(self, request, format=None):
+    def get(self, request: Request, id, format=None)-> Response:
+        user = User.objects.get(id=id)
+        serializer = UserModelSerializer(user)
+        return Response(serializer.data)
+
+
+class UserListAPIView(APIView):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+
+    def get(self, request: Request, format=None)-> Response:
+        #user_detail = UserViewSet.as_view({'get': 'retrieve'})
         users = User.objects.all()
 
         serializer = UserModelSerializer(users, many=True)
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        pass
-
+        serializer = UserModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

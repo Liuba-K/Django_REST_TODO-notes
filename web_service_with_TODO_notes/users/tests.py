7eb2ec2bc4dfc4be698c47abd4f44ee485159667
Liuba <kundasl1@gmail.com>
2022-12-15 01:14:56 +0300

    hw-8 created test APIRequestFactory (user)

--- web_service_with_TODO_notes/users/tests.py
+++ web_service_with_TODO_notes/users/tests.py
@@ -1,3 +1,18 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+#status это константы коды для всех ответов http
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase,APITestCase
+from django.contrib.auth.models import User
+from .views import UserAPIView, UserModelSerializer,UserListAPIView
+from .models import User
 
-# Create your tests here.
+
+class TestUserView(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserListAPIView.as_view({'get': 'list'}) #если mixin
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
